41b140b5ced41fa6a0a8c0c40dce59877dbdf334
zhk-0315 <zhk_0315@163.com>
2023-12-25 19:27:47 +0800

    打开sqlite3 db

--- c/work_srcs/socket/server/cli_sqlite3/cli_sqlite3.c
+++ c/work_srcs/socket/server/cli_sqlite3/cli_sqlite3.c
@@ -1,12 +1,36 @@
+#include <sqlite3.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+#include "pre_modules.h"
+
+static sqlite3* lc_sql = NULL;
 
 int create_cli_sql_database(void)
 {
+    int iret = 0;
+    char fpath[256] = { 0 };
+    char sql_err[256] = { 0 };
+    char sql_cmd[256] = { 0 };
 
+    if (!lc_sql) {
+        return 0;
+    }
 
+    getcwd(fpath, sizeof(fpath));
+    strcat(fpath, "cli_info_sql.db");
 
+    iret = sqlite3_open(fpath, &lc_sql);
+    if (iret != SQLITE_OK) {
+        lc_logout("sqlite3_open cli_info_sql.db error");
+    }
 
+    sprintf(sql_cmd, "CREATE TABLE IF NOT EXISTS INFO(endid int )");
+    iret = sqlite3_exec(lc_sql, sql_cmd, NULL, NULL, (char**)&sql_err);
+    if (iret != SQLITE_OK) {
+        lc_logout("sqlite3_open cli_info_sql.db error");
+    }
 
-
+    return 0;
 }
