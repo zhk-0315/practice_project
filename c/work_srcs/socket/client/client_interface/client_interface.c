6279c187f1c1610bc8ff97ac6f5b4158df3f7963
zhk-0315 <zhk_0315@163.com>
2023-12-28 15:50:03 +0800

    新增interface代码

---
+++ c/work_srcs/socket/client/client_interface/client_interface.c
@@ -0,0 +1,56 @@
+#include <pthread.h>
+
+#include "client_interface.h"
+#include "client_socket.h"
+#include "pre_modules.h"
+#include "unsafe_api.h"
+
+typedef struct interface_info_t {
+    char* info;
+    int (*op_handler)(void);
+} interface_info_t;
+
+static interface_info_t interface_info[] = {
+    { "选择网络模式(TCP/UDP)", switch_socket_type },
+    { "发送消息到server", send_msg_to_server },
+    { "发送消息到其他client", send_msg_to_other_cli }
+};
+
+static void* cli_interface_thread(void* arg)
+{
+    pthread_t tid = pthread_self();
+    int lc_switch = 0;
+    int i = 0;
+    int info_cnt = sizeof(interface_info) / sizeof(interface_info_t);
+
+    pthread_detach(tid);
+    lc_pthread_setname_np(tid, "CliUI");
+
+    while (1) {
+        lc_display_str("欢迎使用client! %s", display_cur_socket_type());
+        for (i = 0; i < info_cnt; i++) {
+            lc_display_str("%d: %s", i, interface_info[i].info);
+        }
+        lc_printf("请选择你的选项: ");
+        lc_scanf("%d", &lc_switch);
+
+        if (interface_info[lc_switch].op_handler) {
+            interface_info[lc_switch].op_handler();
+        }
+    }
+
+    return NULL + 1;
+}
+
+int create_cli_interface_thread(void)
+{
+    int iret = 0;
+    pthread_t tid = 0;
+
+    iret = pthread_create(&tid, NULL, cli_interface_thread, NULL);
+    if (iret != 0) {
+        lc_err_logout("create cli_interface_thread error");
+    }
+
+    return 0;
+}
---
+++ c/work_srcs/socket/client/client_interface/client_interface.h
@@ -0,0 +1,6 @@
+#ifndef __CLIENT__CLIENT_INTERFACE__CLIENT_INTERFACE_H
+#define __CLIENT__CLIENT_INTERFACE__CLIENT_INTERFACE_H
+
+int create_cli_interface_thread(void);
+
+#endif
